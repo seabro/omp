bdcc4800359c910ef3e739889832cd869d49d04c
seabro <seabro@gmail.com>
2024-03-14 13:28:13 +0100

    Test init

--- start.ps1
+++ start.ps1
@@ -4,4 +4,24 @@
     Runspace Author: @DeveloperDurp
     Version        : 1
 #>
-Write-Host "radi"
+
+
+<#
+$progressPreference = 'silentlyContinue'
+$latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
+$latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
+Write-Information "Downloading winget to artifacts directory..."
+Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage $latestWingetMsixBundle
+#>
+
+
+try {
+    winget --version
+}
+catch {
+    Write-Host "Winget not present / outdated"
+
+}
