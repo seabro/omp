d8f3cc5d80fc9fbfaf1283cd7730353c10e9b1a3
seabro <seabro@gmail.com>
2024-03-14 14:17:34 +0100

    Dodavanje postavki

--- start.ps1
+++ start.ps1
@@ -17,18 +17,23 @@ Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
 Add-AppxPackage $latestWingetMsixBundle
 #>
 
+#Set-ExecutionPolicy Bypass -Scope Process -Force;
+#Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
 
-try {
-    winget --version
+if (Get-Command oh-my-posh -errorAction SilentlyContinue) {
+    #oh-my-posh font install --user
+    #Write-Host $PROFILE
+    #oh-my-posh init pwsh | Invoke-Expression
+
+    Add-Content -Path $PROFILE -Value "oh-my-posh init pwsh | Invoke-Expression"
+
 }
-catch {
-    Write-Host "Winget not present / outdated"
-    $progressPreference = 'silentlyContinue'
-    $latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object { $_.EndsWith(".msixbundle") }
-    $latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
-    Write-Information "Downloading winget to artifacts directory..."
-    Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
-    Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-    Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-    Add-AppxPackage $latestWingetMsixBundle
+else {
+    Set-ExecutionPolicy Bypass -Scope Process -Force;
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+    $curUser = ";C:\Users\" + $env:Username + "\AppData\Local\Programs\oh-my-posh\bin"
+    $env:Path += $curUser
 }
+
+#$curUser = ";C:\Users\" + $env:Username + "\AppData\Local\Programs\oh-my-posh\bin"
+#$env:Path += $curUser
